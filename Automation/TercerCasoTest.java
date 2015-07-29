ead7e6435cecfa3541d605f36a4f04acd308e3ea
visego <visego@gmail.com>
2015-07-29 20:28:18 +0200

    Actualizacion_tras_articulo

--- Automation/TercerCasoTest.java
+++ Automation/TercerCasoTest.java
@@ -3,16 +3,17 @@ package Automation;
 import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.*;
- 
+
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class TercerCasoTest {
 
-
 		public static void main(String[] args) {
 			
+			 // Creamos una instancia de FirefoxDriver
 			 WebDriver driver = new FirefoxDriver();
 			 
+			 // Definimos dos variables para almacenar el momento en que comienza y finaliza la prueba
 			 long time_start, time_end;
 			 
 			 String web = "http://www.amazon.es";
@@ -35,14 +36,14 @@ public class TercerCasoTest {
 			//Buscamos el elemento para introducir el usuario
 			 WebElement element;
 			 element = driver.findElement(By.id("ap_email"));
-			 element.sendKeys("visego@gmail.com");
+			 element.sendKeys("aaaa@gmail.com");
 			 
 			 System.out.println("Email introducido");
 			 
 			 //Buscamos el elemento para introducir la  contraseña
 			 WebElement element2;
 			 element2 = driver.findElement(By.id("ap_password"));
-			 element2.sendKeys("Kuuz0nQvoRp9zcg72Nh8");
+			 element2.sendKeys("prueba");
 			 
 			 System.out.println("Contraseña introducida");
 			 
@@ -54,12 +55,8 @@ public class TercerCasoTest {
 			 
 			 time_end = System.currentTimeMillis();
 			 double  time_total= time_end - time_start;
-			 System.out.println("the task has taken "+ (time_total) +" milliseconds");
-			 
-			 double time_total_s = time_total/1000;
-			 System.out.println("the task has taken "+ (time_total_s) +" seconds");
-			
-			// TODO Auto-generated method stub
+			 System.out.println("El tiempo tomado ha sido: "+ (time_total) +" ms");SW
+			 System.out.println("El tiempo tomado ha sido: "+ (time_total/1000) +" s");
 
 		}
 
